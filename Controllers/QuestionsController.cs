3facabc0be113cdc41e1079a1ff5ab3b205c1d95
sarah hachouche <sarahhachouche7@gmail.com>
2023-05-31 15:55:13 +0300

    back1

---
+++ Controllers/QuestionsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuizAPI.Models;
+
+namespace QuizApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly QuizDbContext _context;
+
+        public QuestionsController(QuizDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Questions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Question>>> GetQuestions()
+        {
+            Console.WriteLine(_context);
+            List<object> allQuestions = new List<object>();
+            var random5Qns1 = await (_context.Questions
+                 .Where(x => x.QnId >= 1 && x.QnId <= 10)
+                 .Select(x => new
+                 {
+                    QnId = x.QnId,
+                    QnInWords = x.QnInWords,
+                    ImageName = x.ImageName,
+                    Options = new string[] { x.Option1, x.Option2, x.Option3, x.Option4 }
+                  })
+                 .OrderBy(y => Guid.NewGuid())
+                 .Take(5)
+             ).ToListAsync();
+            allQuestions.AddRange(random5Qns1);
+
+            var random5Qns2 = await (_context.Questions
+                 .Where(x => x.QnId >10 && x.QnId <= 19)
+                 .Select(x => new
+                 {
+                     QnId = x.QnId,
+                     QnInWords = x.QnInWords,
+                     ImageName = x.ImageName,
+                     Options = new string[] { x.Option1, x.Option2, x.Option3, x.Option4 }
+                 })
+                 .OrderBy(y => Guid.NewGuid())
+                 .Take(5)
+             ).ToListAsync();
+            allQuestions.AddRange(random5Qns2);
+
+            var random5Qns3 = await (_context.Questions
+               .Where(x => x.QnId > 19)
+               .Select(x => new
+               {
+                   QnId = x.QnId,
+                   QnInWords = x.QnInWords,
+                   ImageName = x.ImageName,
+                   Options = new string[] { x.Option1, x.Option2, x.Option3, x.Option4 }
+               })
+               .OrderBy(y => Guid.NewGuid())
+               .Take(5)
+           ).ToListAsync();
+            allQuestions.AddRange(random5Qns3);
+
+            return Ok(allQuestions);
+
+
+
+
+        }
+
+        [HttpPost]
+        [Route("GetAnswers")]
+        public async Task<ActionResult<Question>> RetrieveAnswers(int[] qnIds)
+        {
+            var answers = await (_context.Questions
+                .Where(x => qnIds.Contains(x.QnId))
+                .Select(y => new
+                {
+                    QnId = y.QnId,
+                    QnInWords = y.QnInWords,
+                    ImageName = y.ImageName,
+                    Options = new string[] { y.Option1, y.Option2, y.Option3, y.Option4 },
+                    Answer = y.Answer
+                })).ToListAsync();
+            return Ok(answers);
+        }
+
+
+        // PUT: api/Questions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutQuestion(int id, Question question)
+        {
+            if (id != question.QnId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(question).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuestionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Questions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Question>> PostQuestion(Question question)
+        {
+          if (_context.Questions == null)
+          {
+              return Problem("Entity set 'QuizDbContext.Questions'  is null.");
+          }
+            _context.Questions.Add(question);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetQuestion", new { id = question.QnId }, question);
+        }
+
+        // DELETE: api/Questions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteQuestion(int id)
+        {
+            if (_context.Questions == null)
+            {
+                return NotFound();
+            }
+            var question = await _context.Questions.FindAsync(id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            _context.Questions.Remove(question);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool QuestionExists(int id)
+        {
+            return (_context.Questions?.Any(e => e.QnId == id)).GetValueOrDefault();
+        }
+    }
+}
